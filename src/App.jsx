da0bf12349bbfa0bede1a4eea534eba033903718
Bruno <brunofrizzo@outlook.com>
2022-09-20 22:59:54 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from './components/Header';
+import { Post } from './Post';
+
+export function App() {
+    return (
+        <div>
+            <Header/>
+            <Post author="Bruno frizzo" />
+        </div>
+    )
+}
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post(props){
+    return (
+        <div>
+            <p>Post</p>
+            <p>{props.author}</p>
+        </div>
+    );
+}
