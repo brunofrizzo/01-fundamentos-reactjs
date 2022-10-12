77ff111dda13b443e3e3ae894005b1478cba8b0e
Bruno <brunofrizzo@outlook.com>
2022-10-12 19:50:35 -0300

    adding and deleting comments.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,11 @@ import { ThumbsUp, Trash } from 'phosphor-react';
 import { Avatar } from './Avatar';
 import styles from './Comment.module.css';
 
-export function Comment(){
+export function Comment({ content, onDeleteComment }){
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/brunofrizzo.png" />
@@ -16,12 +20,12 @@ export function Comment(){
                             <time title="09 de outubro as 09:00" dateTime="2022-10-09 09:00:00">Cerca de 2h atras</time>
                         </div>
 
-                        <button title="Deleter comentário">
+                        <button onClick={handleDeleteComment} title="Deleter comentário">
                             <Trash size={20} />
                         </button>
                     </header>
 
-                    <p>Muito bom Bruno. Parabens</p>
+                    <p>{content}</p>
                 </div>
 
                 <footer>
